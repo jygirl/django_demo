a7a7b1e53a9dd859da53aa19e2acc639fcae726c
jygirl <shygirl01@sina.com>
2025-02-13 10:57:27 +0800

    update admin，注册models里的数据库

--- erp/apps/erp_test/admin.py
+++ erp/apps/erp_test/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import * # 引入产品表
+
+# 一定要分开逐个注册，不能放在一起
+
+admin.site.register(Goods)
+
+ # 在admin站点中 注册产品表
+
+admin.site.register(GoodsCategory) 
+
+# 在admin站点中 注册产品表
